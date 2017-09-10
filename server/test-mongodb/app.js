95b009f1ffbfa5028afe599e64bd29d7dfc265ce
swang92711@gmail.com <swang92711@gmail.com>
2017-09-09 23:48:35 -0400

    aa

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -11,11 +11,12 @@ module.exports = function(app)
 
   //var connectionString = 'mongodb://webappmaker:webappmaker@ds163181.mlab.com:63181/webappmaker';
   var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+
   if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += 'ds129004.mlab.com:29004/heroku_gln9d69c'; // use yours
+    connectionString += '@ds129004.mlab.com:29004/heroku_gln9d69c'; // use yours
   }
 
 
