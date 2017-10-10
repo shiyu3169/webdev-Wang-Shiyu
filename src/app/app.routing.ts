c092a52e0ce066a9d293a77bd56cb09ba2a70544
Shiyu Wang <swang92711@gmail.com>
2017-10-10 14:52:00 -0400

    routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,20 +9,18 @@ import {TestComponent} from './components/test/test.component';
 import {LoginComponent} from './components/user/login/login.component';
 import {ProfileComponent} from './components/user/profile/profile.component';
 import {RegisterComponent} from './components/user/register/register.component';
-// import {RegisterComponent} from './components/user/register/register.component';
-// import {ProfileComponent} from './components/user/profile/profile.component';
-// import {PageEditComponent} from './components/page/page-edit/page-edit.component';
+import {PageEditComponent} from './components/page/page-edit/page-edit.component';
 import {PageListComponent} from './components/page/page-list/page-list.component';
-// import {PageNewComponent} from './components/page/page-new/page-new.component';
+import {PageNewComponent} from './components/page/page-new/page-new.component';
 import {WebsiteEditComponent} from './components/website/website-edit/website-edit.component';
 import {WebsiteListComponent} from './components/website/website-list/website-list.component';
 import {WebsiteNewComponent} from './components/website/website-new/website-new.component';
-// import {WidgetChooserComponent} from './components/widget/widget-chooser/widget-chooser.component';
-// import {WidgetEditComponent} from './components/widget/widget-edit/widget-edit.component';
-// import {WidgetListComponent} from './components/widget/widget-list/widget-list.component';
-// import {WidgetHeaderComponent} from './components/widget-edit/widget-header/widget-header.component';
-// import {WidgetImageComponent} from './components/widget-edit/widget-image/widget-image.component';
-// import {WidgetYoutubeComponent} from './components/widget-edit/widget-youtube/widget-youtube.component';
+import {WidgetChooserComponent} from './components/widget/widget-chooser/widget-chooser.component';
+import {WidgetEditComponent} from './components/widget/widget-edit/widget-edit.component';
+import {WidgetListComponent} from './components/widget/widget-list/widget-list.component';
+// import {WidgetHeaderComponent} from './components/widget/widget-edit/widget-header/widget-header.component';
+// import {WidgetImageComponent} from './components/widget/widget-edit/widget-image/widget-image.component';
+// import {WidgetYoutubeComponent} from './components/widget/widget-edit/widget-youtube/widget-youtube.component';
 
 const APP_ROUTES: Routes = [
   {path: 'test', component: TestComponent},
@@ -33,12 +31,12 @@ const APP_ROUTES: Routes = [
   {path: 'user/:uid/website', component: WebsiteListComponent},
   {path: 'user/:uid/website/new', component: WebsiteNewComponent},
   {path: 'user/:uid/website/:wid', component: WebsiteEditComponent},
-  // {path: 'user/:uid/website/:wid/page/:pid', component: PageEditComponent},
   {path: 'user/:uid/website/:wid/page', component: PageListComponent},
-  // {path: 'user/:uid/website/:wid/page/new', component: PageNewComponent},
-  // {path: 'user/:uid/website/:wid/page/:pid/widget/new', component: WidgetChooserComponent},
-  // {path: 'user/:uid/website/:wid/page/:pid/widget/:wgid', component: WidgetEditComponent},
-  // {path: 'user/:uid/website/:wid/page/:pid/widget', component: WidgetListComponent},
+  {path: 'user/:uid/website/:wid/page/new', component: PageNewComponent},
+  {path: 'user/:uid/website/:wid/page/:pid', component: PageEditComponent},
+  {path: 'user/:uid/website/:wid/page/:pid/widget', component: WidgetListComponent},
+  {path: 'user/:uid/website/:wid/page/:pid/widget/new', component: WidgetChooserComponent},
+  {path: 'user/:uid/website/:wid/page/:pid/widget/:wgid', component: WidgetEditComponent},
   // {path: 'widget-header', component: WidgetHeaderComponent},
   // {path: 'widget-image', component: WidgetImageComponent},
   // {path: 'widget-youtube', component: WidgetYoutubeComponent}
